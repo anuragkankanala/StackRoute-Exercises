5fd0ca251fd17d1944dd9ac7e2840837f00a6a18
unknown <AN324301@L-777726121.wipro.com>
2015-11-05 17:57:16 +0530

    worked on Bootstrap Project

--- BootStrap/includes/js/script.js
+++ BootStrap/includes/js/script.js
@@ -8,3 +8,10 @@ Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
+
+$(function() {
+  $('#download_alert').click(function(e){
+    e.preventDefault();
+    $('#successAlert').slideDown();
+  });
+});
